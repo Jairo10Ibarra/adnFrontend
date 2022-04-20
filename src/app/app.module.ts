cfd055bbefbe85653e0b4d77179ac9e8535e2b4e
jairo.ibarra <jairo.ibarra@ceiba.com.co>
2022-04-20 14:11:56 -0500

    Se modifican test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from '@home/home.component';
-import { ProductoModule } from '@producto/producto.module';
 import { CoreModule } from '@core/core.module';
 import { CookieService } from 'ngx-cookie-service';
+import { ProductoModule } from '@producto/producto.module';
 
 
 
@@ -18,8 +17,8 @@ import { CookieService } from 'ngx-cookie-service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ProductoModule,
+    AppRoutingModule,
     CoreModule,
   ],
   providers: [CookieService],
