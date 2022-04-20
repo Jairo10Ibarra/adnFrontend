38512a6865a6d4849b9d4779e77baa932f4ac2ca
jairo.ibarra <jairo.ibarra@ceiba.com.co>
2022-04-20 11:25:07 -0500

    :construct: Se elimina el componente de producto

--- src/app/core/components/navbar/navbar.component.spec.ts
+++ src/app/core/components/navbar/navbar.component.spec.ts
@@ -1,5 +1,7 @@
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { ClienteComponent } from '@cliente/components/cliente/cliente.component';
+import { ListarCompraComponent } from '@compra/components/listar-compra/listar-compra.component';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
@@ -8,7 +10,12 @@ describe('NavbarComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ NavbarComponent ],
+      imports: [RouterTestingModule.withRoutes([{
+        path: '/cliente' , component: ClienteComponent
+      }, 
+      { path: '/compra/listar' , component: ListarCompraComponent}
+    ])]
     })
     .compileComponents();
   }));
