dc32654fd1ffeb76b9b328b479319dc448900059
jairo.ibarra <jairo.ibarra@ceiba.com.co>
2022-04-20 12:49:41 -0500

    Se meodifican test

--- src/app/core/components/navbar/navbar.component.spec.ts
+++ src/app/core/components/navbar/navbar.component.spec.ts
@@ -1,8 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ClienteComponent } from '@cliente/components/cliente/cliente.component';
-import { ListarCompraComponent } from '@compra/components/listar-compra/listar-compra.component';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
@@ -12,12 +9,7 @@ describe('NavbarComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NavbarComponent ],
-      imports: [RouterTestingModule.withRoutes([{
-        path: 'cliente' , component: ClienteComponent
-      }, 
-      { path: 'compra/listar' , component: ListarCompraComponent}
-    ])],
-    schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
   }));
